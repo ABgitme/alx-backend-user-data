3d7a5e7268eb97604f46d0757a6031a93f04b9d7
ABgitme <abebekezhabesha@gmail.com>
2024-11-09 07:21:05 +0300

    filtered_logger.py upd

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -132,7 +132,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         """
         Initializes the RedactingFormatter with specified fields to redact.
 
