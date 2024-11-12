245a7d19385240092c67278dde9382a633ea6544
ABgitme <abebekezhabesha@gmail.com>
2024-11-12 06:46:32 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -29,3 +30,28 @@ class BasicAuth(Auth):
 
         # Return the part after "Basic " (space included)
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes a Base64 string.
+
+        Args:
+            base64_authorization_header (str):
+            The Base64 string to decode.
+
+        Returns:
+            str: The decoded string as UTF-8,
+            or None if decoding fails.
+        """
+        if base64_authorization_header is None or\
+                not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            # Decode from Base64
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            # Convert bytes to UTF-8 string
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
