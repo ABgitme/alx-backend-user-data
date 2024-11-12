6bfa6259c296f86f405c4fa5660e791281cca2db
ABgitme <abebekezhabesha@gmail.com>
2024-11-12 05:19:40 +0300

    class to manage the API authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 """
-Authentication module for the API
+auth.py - Authentication module for the API
+
+This module contains the Auth class,
+which provides methods for handling
+authentication, including determining
+if a given path requires authentication,
+retrieving the authorization header,
+and getting the current user.
+
+The Auth class is intended to be a base
+class for more specific authentication
+mechanisms.
 """
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
+    """
+    A base class for API authentication.
+
+    This class provides foundational methods
+    for authentication handling in the API.
+    It includes methods to:
+    - Determine if a specific path requires authentication
+    - Retrieve the authorization header from a request
+    - Identify the current user based on a request
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if a given path requires authentication
