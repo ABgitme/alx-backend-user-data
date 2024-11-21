a257f538fc95583c368f35d35924f331e5081433
ABgitme <abebekezhabesha@gmail.com>
2024-11-21 23:12:40 +0100

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -64,12 +64,12 @@ class DB:
             InvalidRequestError: If the query arguments are invalid.
         """
         if not kwargs:
-            raise InvalidRequestError("No query arguments provided.")
+            raise InvalidRequestError
 
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
-                raise NoResultFound("Not found")
+                raise NoResultFound
             return user
         except InvalidRequestError as e:
-            raise InvalidRequestError(f"Invalid")
+            raise InvalidRequestError
